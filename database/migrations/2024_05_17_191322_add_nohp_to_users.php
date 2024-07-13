a239072d07965172580846d1019171e0aa0c36c0
Rafael Galuh <galprad99@gmail.com>
2024-07-13 15:14:41 +0700

    add back migration

--- database/migrations/2024_05_17_191322_add_nohp_to_users.php
+++ database/migrations/2024_05_17_191322_add_nohp_to_users.php
@@ -13,7 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) { 
+            $table->string('nohp')->nullable();
         });
     }
 
